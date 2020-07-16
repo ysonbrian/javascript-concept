f3126de5bd387af6888d8cf8b5797df1231cf736
ysonbrian <znlsakxls@gmail.com>
2020-07-16 13:32:09 +0900

    basic concept of javascript

---
+++ 2-counter/setup/app.js
@@ -0,0 +1,31 @@
+/// set initial count
+let count = 0;
+
+// select value and buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+	btn.addEventListener('click', function (e) {
+		const styles = e.currentTarget.classList;
+		console.log(styles);
+		if (styles.contains('decrease')) {
+			console.log('in!');
+			count--;
+		} else if (styles.contains('increase')) {
+			count++;
+		} else {
+			count = 0;
+		}
+
+		if (count > 0) {
+			value.style.color = 'green';
+		} else if (count < 0) {
+			value.style.color = 'red';
+		} else {
+			value.style.color = '#222';
+		}
+		value.textContent = count;
+	});
+});
